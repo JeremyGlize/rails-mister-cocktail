182a74349a56b4f53b08c4d31d3f6e11df502c0a
Jeremy Glize <le-schlouze@hotmail.fr>
2020-08-14 16:53:01 +0200

    allyouneedislove

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,7 +4,7 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to cocktails_path
+      redirect_to cocktail_doses_path(@cocktail)
     else
       render "cocktails/show"
     end
