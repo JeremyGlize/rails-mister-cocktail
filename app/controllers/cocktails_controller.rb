6dbff9a7d31b282f7d0055bb67eaee65fccb1316
Jeremy Glize <le-schlouze@hotmail.fr>
2020-08-14 15:55:46 +0200

    modded

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
-class CocktailController < ApplicationController
+class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
   end
 
   def new
